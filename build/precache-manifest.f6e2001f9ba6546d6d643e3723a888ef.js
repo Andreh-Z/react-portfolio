2bcb0f5d8faff5b5b72ce8a7611928b949d069a8
Andreh-Z <witecan931@gmail.com>
2022-11-30 17:16:22 -0300

    some fixes and additions

---
+++ build/precache-manifest.f6e2001f9ba6546d6d643e3723a888ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46be943de4b5a5c8bd037b47465d2a98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebb2564702cad9a677ca",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "8e19a77cad1798a206ef",
+    "url": "/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "ebb2564702cad9a677ca",
+    "url": "/static/js/2.7084b99c.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.7084b99c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e19a77cad1798a206ef",
+    "url": "/static/js/main.0d13b499.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
