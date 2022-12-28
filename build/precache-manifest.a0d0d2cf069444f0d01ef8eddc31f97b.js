0484fed26eacfbdb531055b80170ef5bf1745583
Andreh-Z <witecan931@gmail.com>
2022-12-27 22:06:57 -0300

    build_update

---
+++ build/precache-manifest.a0d0d2cf069444f0d01ef8eddc31f97b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5349a40fa80ed737d3417cfac172c6fc",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "ebb2564702cad9a677ca",
+    "url": "/react-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "60eec2dbd985a1932969",
+    "url": "/react-portfolio/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "ebb2564702cad9a677ca",
+    "url": "/react-portfolio/static/js/2.7084b99c.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-portfolio/static/js/2.7084b99c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60eec2dbd985a1932969",
+    "url": "/react-portfolio/static/js/main.3309a24d.chunk.js"
+  },
+  {
+    "revision": "6f6086f91a7ae7353c00",
+    "url": "/react-portfolio/static/js/runtime-main.873154d3.js"
+  }
+]);
